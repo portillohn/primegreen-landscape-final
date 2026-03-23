f99eccc285c5c88ab2da36b13f1769cbf49c82ca
portillohn <kevinportillous1998@gmail.com>
2026-03-22 21:40:11 -0400

    Instalando Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -115,18 +116,8 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={inter.variable}>
+      <GoogleTagManager gtmId="GTM-WTWV8FZW" />
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WTWV8FZW');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
         <link rel="icon" href="/favicon.jpg" />
         <script
           type="application/ld+json"
@@ -143,7 +134,6 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main>{children}</main>
         <Footer />
